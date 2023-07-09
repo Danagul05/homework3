c67bd4fb355131ca9cb2d9e132ab5331bf6dd04e
Danagul05 <danagul.princess.05@gmail.com>
2023-07-09 23:23:37 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        double[] fractionalNumbers = {1.1,2.2,3.3,4.4,-5.6,6.7,7.8,9.8,-4.5,-6.7,-9.3,4.3,-2.1,-8.1,7.6};
+        double q = 0;
+        int w = 0;
+        boolean n = false;
+        for (double s: fractionalNumbers){
+            if(!n){
+                if (s<0){
+                    n = true;
+        else{
+            if(s>0);
+            q+=s;
+            w++;
+                        System.out.println(q/w);
+                    }
+                }
+                    }
+                }
+            }
+        }
+
+
